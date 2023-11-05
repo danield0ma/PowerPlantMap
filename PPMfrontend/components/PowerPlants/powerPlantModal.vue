b34aee4836e349de6103ca0f950e0ab84171b39b
danield0ma <73988380+danield0ma@users.noreply.github.com>
2023-11-05 15:29:32 +0100

    Add cards and info about the blocs of the powerplant

--- PPMfrontend/components/PowerPlants/powerPlantModal.vue
+++ PPMfrontend/components/PowerPlants/powerPlantModal.vue
@@ -4,12 +4,13 @@
         <div class="shade-content"></div>
         <div class="modal-content" v-on:click.stop>
             <div class="modal-body">
-                <h1 v-if="'name' in this.powerPlant">
-                    Erőmű adatainak szerkesztése
+                <h1 v-if="isEditing">
+                    {{ powerPlant.description }} adatainak szerkesztése
                 </h1>
                 <h1 v-else>Új erőmű létrehozása</h1>
-                <div class="text-left d-flex justify-content-center row g-2">
-                    <div class="col-md-6">
+                <div class="text-left d-flex justify-content-around">
+                    <div class="col-md-5 card">
+                        <h5 class="text-center">Erőmű adatai</h5>
                         <div class="form-group">
                             <label for="id">Erőmű azonosító:</label>
                             <input
@@ -21,7 +22,8 @@
                                 disabled
                             />
                         </div>
-                        <div class="form-floating mb-3">
+                        <div class="form-group">
+                            <label for="name">Erőmű neve:</label>
                             <input
                                 type="text"
                                 class="form-control"
@@ -29,7 +31,6 @@
                                 v-model="powerPlant.name"
                                 required
                             />
-                            <label for="name">Erőmű neve:</label>
                         </div>
                         <div class="form-group">
                             <label for="description">Leírás:</label>
@@ -82,12 +83,12 @@
                             />
                         </div>
                         <div class="form-group">
-                            <label for="color">Szín:</label>
+                            <label for="color">Szín (HTML):</label>
                             <input
                                 type="text"
                                 class="form-control"
                                 id="color"
-                                v-model="powerPlant.color"
+                                v-model="'#' + powerPlant.color"
                                 required
                             />
                         </div>
@@ -102,42 +103,140 @@
                             />
                         </div>
                     </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="color">Szín:</label>
-                            <input
-                                type="text"
-                                class="form-control"
-                                id="color"
-                                v-model="powerPlant.color"
-                                required
-                            />
+                    <div class="col-md-5 card">
+                        <h5 class="text-center">Erőmű blokkjai</h5>
+                        <div v-if="isEditing">
+                            <div
+                                v-for="bloc in powerPlant.blocs"
+                                :key="bloc.blocId"
+                                class="card"
+                            >
+                                <div
+                                    class="d-flex justify-content-center align-items-center"
+                                >
+                                    <h5 class="text-center">
+                                        {{ bloc.blocId }}
+                                    </h5>
+                                    <font-awesome-icon
+                                        :icon="['fas', 'trash']"
+                                        class="faicon red"
+                                    />
+                                </div>
+                                <div class="form-group">
+                                    <label for="blocId">Blokk azonosító:</label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        id="blocId"
+                                        v-model="bloc.blocId"
+                                        required
+                                    />
+                                </div>
+                                <div class="form-group">
+                                    <label for="blocType">Blokk típus:</label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        id="blocType"
+                                        v-model="bloc.blocType"
+                                        required
+                                    />
+                                </div>
+                                <div class="form-group">
+                                    <label for="commissionDate"
+                                        >Blokk üzemének kezdete (év):</label
+                                    >
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        id="commissionDate"
+                                        v-model="bloc.commissionDate"
+                                        required
+                                    />
+                                </div>
+                                <div class="form-group">
+                                    <label for="maxBlocCapacity"
+                                        >Blokk teljesítménye [MW]:</label
+                                    >
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        id="maxBlocCapacity"
+                                        v-model="bloc.maxBlocCapacity"
+                                        required
+                                    />
+                                </div>
+                                <h5 class="text-center">Blokk generátorai</h5>
+                                <div
+                                    v-for="generator in bloc.generators"
+                                    :key="generator.generatorId"
+                                    class="card"
+                                >
+                                    <h5 class="text-center">
+                                        {{ generator.generatorId }}
+                                    </h5>
+                                    <div class="form-group">
+                                        <label for="generatorId"
+                                            >Generátor azonosítója:</label
+                                        >
+                                        <input
+                                            type="text"
+                                            class="form-control"
+                                            id="generatorId"
+                                            v-model="generator.generatorId"
+                                            required
+                                        />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="maxCapacity"
+                                            >Generátor maximális teljesítménye
+                                            [MW]:</label
+                                        >
+                                        <input
+                                            type="text"
+                                            class="form-control"
+                                            id="maxCapacity"
+                                            v-model="generator.maxCapacity"
+                                            required
+                                        />
+                                    </div>
+                                    <button
+                                        class="btn btn-success d-flex justify-content-center align-items-center p-0"
+                                    >
+                                        Generátor hozzáadása
+                                        <font-awesome-icon
+                                            :icon="['fas', 'plus']"
+                                            class="faicon white"
+                                        />
+                                    </button>
+                                </div>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="address">Cím:</label>
-                            <input
-                                type="text"
-                                class="form-control"
-                                id="address"
-                                v-model="powerPlant.address"
-                                required
+                        <div v-else class="pt-3"></div>
+                        <button
+                            class="btn btn-success d-flex justify-content-center align-items-center p-0 ml-2 mr-2"
+                        >
+                            Blokk hozzáadása
+                            <font-awesome-icon
+                                :icon="['fas', 'plus']"
+                                class="faicon white"
                             />
-                        </div>
+                        </button>
                     </div>
                 </div>
                 <div>
                     <button
+                        v-on:click="$emit('close')"
+                        class="btn btn-outline-secondary mr-3"
+                    >
+                        Bezárás
+                    </button>
+                    <button
                         type="submit"
-                        class="btn btn-success"
+                        class="btn btn-success ml-3"
                         v-on:click="savePowerPlant"
                     >
                         Mentés
-                    </button>
-                    <button
-                        v-on:click="$emit('close')"
-                        class="btn btn-outline-secondary"
-                    >
-                        Bezárás
                     </button>
                 </div>
             </div>
@@ -152,21 +251,26 @@ export default {
         powerPlant: Object,
     },
 
-    // data() {
-    //     return {
-    //         Id: "",
-    //         Name: "",
-    //         Description: "",
-    //         OperatorCompany: "",
-    //         Webpage: "",
-    //         Longitude: "",
-    //         Latitude: "",
-    //         Color: "",
-    //         Address: "",
-    //         IsCountry: "",
-    //         Blocs: [],
-    //     };
-    // },
+    data() {
+        return {
+            isEditing: false,
+            //         Id: "",
+            //         Name: "",
+            //         Description: "",
+            //         OperatorCompany: "",
+            //         Webpage: "",
+            //         Longitude: "",
+            //         Latitude: "",
+            //         Color: "",
+            //         Address: "",
+            //         IsCountry: "",
+            //         Blocs: [],
+        };
+    },
+
+    mounted() {
+        this.isEditing = "name" in this.powerPlant;
+    },
 
     methods: {
         savePowerPlant() {
@@ -184,6 +288,29 @@ export default {
 </script>
 
 <style>
+.card {
+    background-color: white;
+    border: 1px solid #333;
+    box-shadow: #333 0px 0px 3px;
+    border-radius: 15px;
+    padding: 1rem 0.5rem;
+    margin: 1rem 0.5rem;
+}
+
+.faicon {
+    cursor: pointer;
+    vertical-align: center;
+    padding: 0.5rem;
+}
+
+.white {
+    color: white;
+}
+
+h5 {
+    margin: 0;
+}
+
 .modal-backdrop {
     position: fixed;
     top: 0;
@@ -198,7 +325,7 @@ export default {
 .modal-content {
     background-color: #f5f5f5;
     color: #000;
-    padding: 20px;
+    padding: 10px;
     border-radius: 15px;
     border: 3px solid #333;
     position: fixed;
@@ -207,16 +334,16 @@ export default {
     transform: translate(-50%, -50%);
     box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
     opacity: 1;
-    max-width: 90%;
+    max-width: 85%;
     max-height: 90%;
     z-index: 1000;
     text-align: center;
 }
 
 .modal-body {
-    max-height: calc(90vh - 40px); /* 90vh minus the total vertical padding */
+    max-height: calc(90vh - 20px);
     overflow: auto;
-    padding: 20px;
+    padding: 10px;
 }
 
 .shade-content {
@@ -229,7 +356,11 @@ export default {
     left: 50%;
     transform: translate(-50%, -50%);
     box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-    max-width: 90%;
+    max-width: 85%;
     z-index: 999;
+}
+
+.red {
+    color: red;
 }
 </style>
