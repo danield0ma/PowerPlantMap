8f7e6529d7403f7a0ac58dc7101fc72d8bd010a3
danield0ma <73988380+danield0ma@users.noreply.github.com>
2023-11-16 00:09:25 +0100

    Add Matomo again

--- PPMfrontend/components/Headers/PpmHeader.vue
+++ PPMfrontend/components/Headers/PpmHeader.vue
@@ -21,6 +21,24 @@
 <script>
 export default {
     name: "PpmHeader",
+
+    mounted() {
+        // Matomo
+        var _paq = (window._paq = window._paq || []);
+        _paq.push(["trackPageView"]);
+        _paq.push(["enableLinkTracking"]);
+        (function () {
+            var u = "https://hallgato0.matomo.cloud/";
+            _paq.push(["setTrackerUrl", u + "matomo.php"]);
+            _paq.push(["setSiteId", "1"]);
+            var d = document,
+                g = d.createElement("script"),
+                s = d.getElementsByTagName("script")[0];
+            g.async = true;
+            g.src = "//cdn.matomo.cloud/hallgato0.matomo.cloud/matomo.js";
+            s.parentNode.insertBefore(g, s);
+        })();
+    },
 };
 </script>
 
